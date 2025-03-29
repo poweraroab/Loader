d7084959446992cbc831aee6267e68f9c2f6f320
poweraroab <67917872+poweraroab@users.noreply.github.com>
2025-03-29 17:01:59 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -20,48 +20,47 @@ local function wrapFuncGlobal(func, customFenv)
 
 	return setfenv(func, fenv)
 end
-
 local function fetchFile(path, branch)
-	branch = (branch or "main")
-	local result = (
-		if not wrapperEnv.DEV_MODE then
-			http_request({
-				Url = string.format("https://raw.githubusercontent.com/%s/%s/%s/%s", repoOwner, repoName, branch, path),
-				Method = "GET",
-				Headers = {
-					["Content-Type"] = "text/html; charset=utf-8",
-				}
-			})
-		else {Success = true}
-	)
-	local srcFile = (if (result.Success) then result.Body else nil)
-	local sepPath = string.split(path, "/")
-	table.insert(sepPath, 1, branch)
-	local currentPath = repoName
+    branch = (branch or "main")
+    local result = (
+        if not wrapperEnv.DEV_MODE then
+            http_request({
+                Url = string.format("https://raw.githubusercontent.com/poweraroab/Loader/%s/%s", branch, path),
+                Method = "GET",
+                Headers = {
+                    ["Content-Type"] = "text/html; charset=utf-8",
+                }
+            })
+        else {Success = true}
+    )
+    local srcFile = (if (result.Success) then result.Body else nil)
+    local sepPath = string.split(path, "/")
+    table.insert(sepPath, 1, branch)
+    local currentPath = repoName
 
-	for pathIndex, pathStr in sepPath do
-		if pathIndex == #sepPath then
-			currentPath ..= ("/" .. pathStr)
-			local localSrcFile = (if isfile(currentPath) then readfile(currentPath) else nil)
+    for pathIndex, pathStr in sepPath do
+        if pathIndex == #sepPath then
+            currentPath ..= ("/" .. pathStr)
+            local localSrcFile = (if isfile(currentPath) then readfile(currentPath) else nil)
 
-			if (wrapperEnv.DEV_MODE or not result.Success) then -- if DEV_MODE or file fetch failed then we load local file
-				if localSrcFile then
-					srcFile = localSrcFile
-					warn(string.format("Loading local file '%s' from branch `%s`.", path, branch))
-				else
-					warn(string.format("Failed to load `%s` of branch `%s` from the repository.", path, branch))
-				end
-			else -- loads the fetched file online
-				if (localSrcFile ~= srcFile) then
-					writefile(currentPath, srcFile)
-				end
-			end
-		else
-			currentPath ..= ("/" .. pathStr)
-			if not isfolder(currentPath) then makefolder(currentPath) end
-		end
-	end
-	return (result.Success), srcFile
+            if (wrapperEnv.DEV_MODE or not result.Success) then
+                if localSrcFile then
+                    srcFile = localSrcFile
+                    warn(string.format("Loading local file '%s' from branch `%s`.", path, branch))
+                else
+                    warn(string.format("Failed to load `%s` of branch `%s` from the repository.", path, branch))
+                end
+            else
+                if (localSrcFile ~= srcFile) then
+                    writefile(currentPath, srcFile)
+                end
+            end
+        else
+            currentPath ..= ("/" .. pathStr)
+            if not isfolder(currentPath) then makefolder(currentPath) end
+        end
+    end
+    return (result.Success), srcFile
 end
 
 local function import(path, branch)
@@ -100,7 +99,7 @@ do -- environment init
 	wrapperEnv["import"] = import
 	wrapperEnv["fetchFile"] = fetchFile
 	--wrapperEnv["loadAsset"] = loadAsset
-	wrapperEnv["DEV_MODE"] = DEV_MODE
+	wrapperEnv["DEV_MODE"] = false
 end
 
-return import("main/main.lua")(...)
+return import("src/main.lua")(...)
